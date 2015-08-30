f7b31fd9e0ebd52851f9963f3e8c6ee154fbf730
xqq <xqq@0ginr.com>
2015-08-30 18:05:06 +0800

    RPN: Support mul / div

--- RPN.hpp
+++ RPN.hpp
@@ -12,15 +12,22 @@ public:
         std::stack<Token::Ref> stack;
 
         for (auto& token : list) {
-            if (token->GetTag() == Tags::kNum || token->GetTag() == Tags::kReal) {
+            int tag = token->GetTag();
+
+            if (tag == Tags::kNum || tag == Tags::kReal) {
                 stack.push(token);
-            } else if (token->GetTag() == Tags::kAdd || token->GetTag() == Tags::kSub) {
+            } else if (tag == Tags::kAdd || tag == Tags::kSub || tag == Tags::kMul || tag == Tags::kDiv) {
                 Token::Ref top1 = stack.top(); stack.pop();
                 Token::Ref top2 = stack.top(); stack.pop();
                 if (top1->IsNumber() && top2->IsNumber()) {
                     float value2 = top1->GetValue();
                     float value1 = top2->GetValue();
-                    float result = token->GetTag() == Tags::kAdd ? value1 + value2 : value1 - value2;
+                    float result = 0;
+                    if (tag == Tags::kAdd || tag == Tags::kSub) {
+                        result = tag == Tags::kAdd ? value1 + value2 : value1 - value2;
+                    } else if (tag == Tags::kMul || tag == Tags::kDiv) {
+                        result = tag == Tags::kMul ? value1 * value2 : value1 / value2;
+                    }
                     stack.push(Real::Create(result));
                 } else {
                     throw std::runtime_error("Eval error: Unexcepted stack top element");
